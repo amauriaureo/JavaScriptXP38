31cb0b1b14b3bfb6ea5e90270e93d152b5c60cae
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-06 15:54:15 -0300

    sim

--- avancado/projeto/app.js
+++ avancado/projeto/app.js
@@ -1,13 +1,20 @@
 // redeline-sync: receber entrada de pessoas usuarias. simular . no frton é equivalente a input.value
-const livros = require('./database')
-// console.log(livros)
 
 //pegar o input
+// Se sim, mostra categorias disponíveis e pergunta qual será escolhida.
+// Se não, mostra todos os livros em ordem crescente pela quantidade de páginas
 
+const livros = require('./database')
 const  readline = require('readline-sync')
 
 const entradaInicial = readline.question('Deseja Buscar um livro? S/N')
 
-// Se sim, mostra categorias disponíveis e pergunta qual será escolhida.
+if(entradaInicial.toLocaleUpperCase() === 'S') {
+    console.log('Essas são as categorias disponíveis:')
+    console.log('Produtividade', '/História brasileira','/Américas','/Estilo de vida','/Tecnologia')
 
-// Se não, mostra todos os livros em ordem crescente pela quantidade de páginas
+    const entradaCategoria = readline.question('Qual categoria você escolhe?')
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria)
+
+    console.table(retorno)
+}
